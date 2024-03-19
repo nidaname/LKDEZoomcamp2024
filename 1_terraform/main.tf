01741801c76a851f97819273fecd49da0080671d
nidaname <63225009+nidaname@users.noreply.github.com>
2024-03-19 07:16:37 +0000

    terraform main edits and variables

--- 1_terraform/main.tf
+++ 1_terraform/main.tf
@@ -8,13 +8,14 @@ terraform {
 }
 
 provider "google" {
-  project = "onyx-descent-417702"
-  region  = "asia-southeast1"
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.project_region
 }
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "onyx-descent-417702-terra-bucket"
-  location      = "ASIA"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -25,4 +26,9 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
---
+++ 1_terraform/variables.tf
@@ -0,0 +1,30 @@
+variable "credentials" {
+  description = "credentials for GCP Project"
+  default     = "/workspaces/LKDEZoomcamp2024/1_terraform/keys/terraform_sa.json"
+}
+
+variable "project" {
+  description = "GCP Project"
+  default     = "onyx-descent-417702"
+}
+
+variable "project_region" {
+  description = "region for GCP Project"
+  default     = "asia-southeast1"
+}
+
+variable "location" {
+  description = "location of any GCS resources"
+  default     = "ASIA"
+}
+
+variable "bq_dataset_name" {
+  description = "BQ Dataset name"
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "GCS Bucket name"
+  default     = "onyx-descent-417702-terra-bucket"
+}
+
